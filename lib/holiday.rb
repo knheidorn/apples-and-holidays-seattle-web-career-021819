1a850aa10f0b5fee1baf140a6bbd8739b5b6dd95
Kim Heidorn <heidorkn@gmail.com>
2019-02-10 21:08:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,6 +24,7 @@ def all_winter_holiday_supplies(holiday_hash)
   array = holiday_hash[:winter].map do |holiday, supply|
     supply
   end
+  array
 end
 
 def all_supplies_in_holidays(holiday_hash)
